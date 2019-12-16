883a6413afdf45335d91115ae59ecf4e3712dabd
cleiveliu <cleiveliu1@gmail.com>
2019-12-16 17:32:09 +0800

    add to article.views

--- django_blog_proj/article/views.py
+++ django_blog_proj/article/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect, get_list_or_404, reverse
+from .forms import ArticleForm
+from .models import Article, Comment
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def article(request):
+
+    keyword = request.GET.get("keyword")
+
+    if keyword:
+        articles = Article.objects.filter(title__contains=keyword)
+        return render(request, "articles.html", {"articles": articles})
+    articles = Article.objects.all()
+
+    return render(request, "articles.html", {"articles": articles})
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+@login_required(login_url="user:login")
+def dashboard(request):
+    articles = Article.objects.filter(auth=request.user)
+    context = {"articles": articles}
+    return render(request, "dashboard.html", context)
+
