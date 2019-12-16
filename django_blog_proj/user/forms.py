f1fc635c16c2c46d14ef0284238ea10c69ca1b27
cleiveliu <cleiveliu1@gmail.com>
2019-12-16 22:48:32 +0800

    fix some bugs

--- django_blog_proj/user/forms.py
+++ django_blog_proj/user/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="username")
-    password = forms.CharField(label="password")
+    password = forms.CharField(label="password", widget = forms.PasswordInput)
 
 
 class RegisterForm(forms.Form):
