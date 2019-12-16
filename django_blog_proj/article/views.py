2c7e5f258d3f5ad04c1b8b399f442e1c4137f783
cleiveliu <cleiveliu1@gmail.com>
2019-12-16 18:05:45 +0800

    add article.views

--- django_blog_proj/article/views.py
+++ django_blog_proj/article/views.py
@@ -31,3 +31,67 @@ def dashboard(request):
     context = {"articles": articles}
     return render(request, "dashboard.html", context)
 
+
+@login_required(login_url="user:login")
+def add_article(request):
+    form = ArticleForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        article = form.save(commit=False)
+
+        article.author = request.user
+        article.save()
+
+        messages.success(request, "文章创建成功")
+        return redirect("article:dashboard")
+    return render(request, "articles.html", {"form": form})
+
+
+def detail(request, id):
+    article = get_list_or_404(Article, id=id)
+    comments = article.comments.all()
+    context = {"article": article, "comments": comments}
+
+    return render(request, "detail.html", context)
+
+
+@login_required(login_url="user:login")
+def update_article(request, id):
+    article = get_list_or_404(Article, id=id)
+    form = ArticleForm(request.POST or None, request.FILES or None, instance=article)
+
+    if form.is_valid():
+        article = form.save(commit=False)
+
+        article.author = request.user
+        article.save()
+
+        messages.success(request, "文章已成功更新")
+        return redirect("article:dashboard")
+    return render(request, "update.html", {"form": form})
+
+
+@login_required(login_url="user:login")
+def delete_article(request, id):
+    article = get_list_or_404(Article, id=id)
+    article.delete()
+    messages.success(request, "文章成功删除")
+
+    return redirect("article:dashboard")
+
+
+def add_comment(request, id):
+    article = get_list_or_404(Article, id=id)
+    if request.method == "POST":
+        comment_author = request.POST.get("comment_author")
+        comment_content = request.POST.get("comment_content")
+
+        newComment = Comment(
+            comment_author=comment_author, comment_content=comment_content
+        )
+
+        newComment.article = article
+
+        newComment.save()
+    return redirect(reverse("article:detail", kwargs={"id": id}))
+
